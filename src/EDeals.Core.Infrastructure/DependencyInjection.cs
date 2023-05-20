20dfed904ff9bd99df50e05809d001ec62884efc
Andrei Ene <95134095+AndrewSSB@users.noreply.github.com>
2023-05-21 01:35:34 +0300

    - created the architecture of the project - added logging

---
+++ src/EDeals.Core.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace EDeals.Core.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureMethods(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
--- src/EDeals.Core/Program.cs
+++ src/EDeals.Core/Program.cs
@@ -1,25 +1,53 @@
-var builder = WebApplication.CreateBuilder(args);
+using EDeals.Core.Application;
+using EDeals.Core.Infrastructure;
+using Serilog;
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    // Add Services
+    builder.Services
+        .AddApplicationMethods()
+        .AddInfrastructureMethods();
+
+    // Add Logging
+    builder.Host.UseSerilog((context, configuration) =>
+        configuration.ReadFrom.Configuration(context.Configuration));
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseSerilogRequestLogging();
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex) when (ex is not OperationCanceledException && !ex.GetType().Name.Contains("StopTheHostException") && !ex.GetType().Name.Contains("HostAbortedException"))
+{
+    Log.Fatal(ex, "Unhandled exception in Program");
+}
+finally
+{
+    Log.Information("App shut down complete");
+    Log.CloseAndFlush();
+}
