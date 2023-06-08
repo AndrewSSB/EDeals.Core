25dd0460524dd867569b2ce0b0b203b00e0be337
Andrei Ene <95134095+AndrewSSB@users.noreply.github.com>
2023-06-08 22:41:38 +0300

    afas

--- src/EDeals.Core/Extensions/ApiExtensions.cs
+++ src/EDeals.Core/Extensions/ApiExtensions.cs
@@ -4,6 +4,7 @@
     {
         public static void AddApplicationSettings(WebApplicationBuilder builder)
         {
+            //trigger
             builder.Configuration.AddJsonFile("appsettings.json", false, true);
             builder.Configuration.AddJsonFile("appsettings.Local.json", true, true);
             builder.Configuration.AddEnvironmentVariables();
